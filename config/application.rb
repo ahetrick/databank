e48a85b485112ee130f2fcd84b99dcca10a4059f
Colleen Fallaw <mfall3@illlinois.edu>
2015-08-14 11:25:12 -0500

    autoload paths so app can find needed files

--- config/application.rb
+++ config/application.rb
@@ -70,6 +70,7 @@ module Databank
     config.autoload_paths << File.join(Rails.root, 'jobs')
     config.autoload_paths << File.join(Rails.root, 'lib')
     config.autoload_once_paths << File.join(Rails.root, 'app/models')
+    config.autoload_once_paths << File.join(Rails.root, 'app/models/concerns')
 
   end
 end
