bc3a569e24cf26ca336415eb879725c5eaff4cf2
Colleen Fallaw <mfall3@illlinois.edu>
2015-12-01 10:37:29 -0600

    remove commented out unused code

--- app/jobs/create_datafile_from_remote_job.rb
+++ app/jobs/create_datafile_from_remote_job.rb
@@ -34,22 +34,12 @@ class CreateDatafileFromRemoteJob < ProgressJob::Base
       Net::HTTP.start(uri.host,uri.port, :use_ssl => (uri.scheme == 'https')  ){ |http|
         http.request_get(uri.path){ |res|
 
-          # # Works with the response object as well:
-          # res.each_header do |header_name, header_value|
-          #   Rails.logger.warn "#{header_name} : #{header_value}"
-          # end
-
-          # seg_count = 1
-
           res.read_body{ |seg|
-            #Rails.logger.warn "seg_count: #{seg_count}"
             outfile << seg
-            #seg_count = seg_count + 1
             update_progress()
           }
         }
       }
-
 
     end
 
@@ -59,62 +49,7 @@ class CreateDatafileFromRemoteJob < ProgressJob::Base
       @datafile.save!
     end
 
-
-
-    # uri = URI.parse(@remote_url)
-    # Net::HTTP.start(uri.host,uri.port){ |http|
-    #   http.request_get(uri.path){ |res|
-    #     res.read_body{ |seg|
-    #       outfile << seg
-    #       #hack -- adjust to suit:
-    #       #sleep 0.005
-    #     }
-    #   }
-    # }
-
-
-    # f = open("/public/uploads/test.txt", "wb+")
-    #
-    # begin
-    #   http.request_get(@remote_url) do |resp|
-    #     resp.read_body do |segment|
-    #       f.write(segment)
-    #     end
-    #   end
-    # ensure
-    #   f.close()
-    # end
-
-    # @progress_max.times do |count|W
-    #   update_progress
-    # end
-
-
-
   end
 
 end
 
-
-
-# class CreateDatafileFromRemoteJob < ProgressJob::Base
-#   # queue_as :default
-#
-#   def initialize(dataset_id, remote_url, progress_max)
-#     @remote_url = remote_url
-#     @dataset_id = dataset_id
-#     super progress_max: Integer(progress_max)
-#   end
-#
-#   def perform
-#
-#    100.times do |count|
-#
-#       update_progress
-#     end
-#
-#    Datafile.create(:remote_binary_url => @remote_url, :dataset_id => @dataset_id)
-#
-#   end
-#
-# end
