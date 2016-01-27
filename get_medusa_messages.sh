2ad8cd18b3b57a34491ad5a99e29282abc9cf460
Colleen Fallaw <mfall3@illlinois.edu>
2016-01-27 13:13:20 -0600

    refine get medusa messages test scipt

--- get_medusa_messages.sh
+++ get_medusa_messages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 rvm use 2.2.1@idb_v1
 
 touch log/get_medusa_messages.log
-echo $(date -u) >> log/get_medusa_message.log
+echo $(date -u) >> log/get_medusa_messages.log
