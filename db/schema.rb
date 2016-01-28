52e940059dc975815392e71d0c70888f23b20a7e
Colleen Fallaw <mfall3@illlinois.edu>
2016-01-28 08:47:18 -0600

    refine dataset status flags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126193536) do
+ActiveRecord::Schema.define(version: 20160128144433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,9 +65,10 @@ ActiveRecord::Schema.define(version: 20160126193536) do
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
     t.string   "keywords"
-    t.boolean  "has_datacite_change",         default: true
+    t.boolean  "has_datacite_change",         default: false
     t.string   "publication_state",           default: "draft"
     t.string   "version",                     default: "1"
+    t.boolean  "curator_hold",                default: false
   end
 
   add_index "datasets", ["key"], name: "index_datasets_on_key", unique: true, using: :btree
