df11c2b778864b77f68fca67127ba2cedf2bcd34
Colleen Fallaw <mfall3@illlinois.edu>
2016-02-03 12:23:33 -0600

    rough in new welcome graphic

--- app/models/databank_mailer.rb
+++ app/models/databank_mailer.rb
@@ -4,7 +4,7 @@ class DatabankMailer < ActionMailer::Base
   def confirm_deposit(dataset_key)
     @dataset = Dataset.where(key: dataset_key).first
     if @dataset
-      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email, 'colleenfallaw@yahoo.com'], subject: '[Illinois Data Bank] Dataset successfully deposited')
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], subject: '[Illinois Data Bank] Dataset successfully deposited')
     else
       Rails.logger.warn "Confirmation email not sent because dataset not found for key: #{dataset_key}."
     end
