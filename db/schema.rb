2784d8dabff0d0003d10a88659ebbd1324e797a0
Colleen Fallaw <mfall3@illlinois.edu>
2016-02-04 15:20:47 -0600

    add relase date to datasets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203201512) do
+ActiveRecord::Schema.define(version: 20160204212025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160203201512) do
     t.string   "publication_state",           default: "draft"
     t.string   "version",                     default: "1"
     t.boolean  "curator_hold",                default: false
+    t.date     "release_date"
   end
 
   add_index "datasets", ["key"], name: "index_datasets_on_key", unique: true, using: :btree
