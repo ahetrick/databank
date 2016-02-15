ea8bad38a9c19aff11dd40b54d6143f6531ef402
Colleen Fallaw <mfall3@illlinois.edu>
2016-02-15 15:51:44 -0600

    update deposit email to copy databank email address

--- app/models/databank_mailer.rb
+++ app/models/databank_mailer.rb
@@ -4,7 +4,7 @@ class DatabankMailer < ActionMailer::Base
   def confirm_deposit(dataset_key)
     @dataset = Dataset.where(key: dataset_key).first
     if @dataset
-      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], subject: '[Illinois Data Bank] Dataset successfully deposited')
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], cc: 'databank@library.illinois.edu',  subject: '[Illinois Data Bank] Dataset successfully deposited')
     else
       Rails.logger.warn "Confirmation email not sent because dataset not found for key: #{dataset_key}."
     end
@@ -13,7 +13,7 @@ class DatabankMailer < ActionMailer::Base
   def confirm_deposit_update(dataset_key)
     @dataset = Dataset.where(key: dataset_key).first
     if @dataset
-      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], subject: '[Illinois Data Bank] Dataset successfully updated')
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], cc: 'databank@library.illinois.edu', subject: '[Illinois Data Bank] Dataset successfully updated')
     else
       Rails.logger.warn "Update confirmation email not sent because dataset not found for key: #{dataset_key}."
     end
