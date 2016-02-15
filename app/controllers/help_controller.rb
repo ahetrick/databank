ff531766a33681f037e0f7194024c31f4de130ee
Colleen Fallaw <mfall3@illlinois.edu>
2016-02-15 15:31:06 -0600

    correct syntax error

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -8,6 +8,6 @@ class HelpController < ApplicationController
       help_request = DatabankMailer.contact_help(params)
       help_request.deliver_now
     end
-    redirect_to '/help', notice: "Email has been sent to the Research Data Service Team.  A confirmation copy will be sent to #{ @params['help-email'] } "
+    redirect_to '/help', notice: "Email has been sent to the Research Data Service Team.  A confirmation copy will be sent to #{ params['help-email'] } "
   end
 end
