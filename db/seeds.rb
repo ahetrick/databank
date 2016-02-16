58bb12ee5bc8fd50cb38b2eaa5052212cd8c0552
Colleen Fallaw <mfall3@illlinois.edu>
2016-02-16 10:56:06 -0600

    update funder select order

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,85 +34,78 @@ end
 funder_idoe = FunderInfo.find_or_initialize_by(code: "IDOE")
 funder_idoe.name = "DOE"
 funder_idoe.identifier = "10.13039/100000015"
-funder_idoe.display_position = 1
+funder_idoe.display_position = 10
 funder_idoe.identifier_scheme = "DOI"
 funder_idoe.save!
 
 funder_idceo = FunderInfo.find_or_initialize_by(code: "IDCEO")
 funder_idceo.name = "IL Department of Commerce & Economic Opportunity (DCEO)"
 funder_idceo.identifier = "10.13039/100004885"
-funder_idceo.display_position = 2
+funder_idceo.display_position = 20
 funder_idceo.identifier_scheme = "DOI"
 funder_idceo.save!
 
 funder_idhs = FunderInfo.find_or_initialize_by(code: "IDHS")
 funder_idhs.name = "IL Department of Human Services (DHS)"
 funder_idhs.identifier = "10.13039/100004886"
-funder_idhs.display_position = 3
+funder_idhs.display_position = 30
 funder_idhs.identifier_scheme = "DOI"
 funder_idhs.save!
 
 funder_idnr = FunderInfo.find_or_initialize_by(code: "IDNR")
 funder_idnr.name = "IL Department of Natural Resources (IDNR)"
 funder_idnr.identifier = "10.13039/100004887"
-funder_idnr.display_position = 4
+funder_idnr.display_position = 40
 funder_idnr.identifier_scheme = "DOI"
 funder_idnr.save!
-
-# funder_idot = FunderInfo.find_or_initialize_by(code: "IDOT")
-# funder_idot.name = "IL Department of Transportation (IDOT)"
-# funder_idot.identifier = ""
-# funder_idot.display_position = 5
-# funder_idot.identifier_scheme = ""
-# funder_idot.save!
-
-funder_idot = FunderInfo.find_or_initialize_by(code: "USGS")
-funder_idot.name = "USGS"
-funder_idot.identifier = "10.13039/100000203"
-funder_idot.display_position = 5
-funder_idot.identifier_scheme = "DOI"
-funder_idot.save!
-
 
 funder_nasa = FunderInfo.find_or_initialize_by(code: "NASA")
 funder_nasa.name = "NASA"
 funder_nasa.identifier = "10.13039/100000104"
-funder_nasa.display_position = 6
+funder_nasa.display_position = 50
 funder_nasa.identifier_scheme = "DOI"
 funder_nasa.save!
 
 funder_nih = FunderInfo.find_or_initialize_by(code: "NIH")
 funder_nih.name = "NIH"
 funder_nih.identifier = "10.13039/100000002"
-funder_nih.display_position = 7
+funder_nih.display_position = 60
 funder_nih.identifier_scheme = "DOI"
 funder_nih.save!
 
 funder_nsf = FunderInfo.find_or_initialize_by(code: "NSF")
 funder_nsf.name = "NSF"
 funder_nsf.identifier = "10.13039/100000001"
-funder_nsf.display_position = 8
+funder_nsf.display_position = 70
 funder_nsf.identifier_scheme = "DOI"
 funder_nsf.save!
 
 funder_usarmy = FunderInfo.find_or_initialize_by(code: "USARMY")
 funder_usarmy.name = "US Army"
 funder_usarmy.identifier = "10.13039/100006751"
-funder_usarmy.display_position = 9
+funder_usarmy.display_position = 80
 funder_usarmy.identifier_scheme = "DOI"
 funder_usarmy.save!
 
 funder_usda = FunderInfo.find_or_initialize_by(code: "USDA")
 funder_usda.name = "USDA"
 funder_usda.identifier = "10.13039/100000199"
-funder_usda.display_position = 10
+funder_usda.display_position = 90
 funder_usda.identifier_scheme = "DOI"
 funder_usda.save!
+
+funder_idot = FunderInfo.find_or_initialize_by(code: "USGS")
+funder_idot.name = "USGS"
+funder_idot.identifier = "10.13039/100000203"
+funder_idot.display_position = 100
+funder_idot.identifier_scheme = "DOI"
+funder_idot.save!
+
 
 funder_other = FunderInfo.find_or_initialize_by(code: "other")
 funder_other.name = "Other -- Please provide name:"
 funder_other.identifier = ""
-funder_other.display_position = 11
+funder_other.display_position = 1000
 funder_other.identifier_scheme = ""
 funder_other.save!
 
