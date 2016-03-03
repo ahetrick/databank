8796a25950cf1db04a0ba3c3dfcfb1d5aa854bf5
Colleen Fallaw <mfall3@illlinois.edu>
2016-03-03 14:49:53 -0600

    refine help mail system

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,5 +1,10 @@
 class HelpController < ApplicationController
   def index
+    # Rails.logger.warn params.to_yaml
+    if params.has_key?('key')
+      @dataset = Dataset.find_by_key(params['key'])
+    end
+    # Rails.logger.warn @dataset.to_yaml
   end
   def sensitive
   end
