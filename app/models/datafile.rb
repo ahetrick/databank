6af021a3c00ad0f99df27fa9aaa70158be7493ac
Colleen Fallaw <mfall3@illlinois.edu>
2016-03-11 13:47:54 -0600

    refine content negotiation

--- app/models/datafile.rb
+++ app/models/datafile.rb
@@ -18,7 +18,7 @@ class Datafile < ActiveRecord::Base
   end
 
   def as_json(options={})
-    super(:only => [:web_id,:binary,:binary_name,:binary_size,:medusa_id,:created_at,:updated_at] )
+    super(:only => [:web_id,:binary_name,:binary_size,:medusa_id,:created_at,:updated_at] )
   end
 
   def bytestream_name
