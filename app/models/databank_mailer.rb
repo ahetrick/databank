72670a165455ecb18b8e62655b3350ed7e1b79f0
Colleen Fallaw <mfall3@illlinois.edu>
2016-03-17 14:44:08 -0500

    refine interface

--- app/models/databank_mailer.rb
+++ app/models/databank_mailer.rb
@@ -4,7 +4,7 @@ class DatabankMailer < ActionMailer::Base
   def confirm_deposit(dataset_key)
     @dataset = Dataset.where(key: dataset_key).first
     if @dataset
-      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], cc: 'databank@library.illinois.edu',  subject: '[Illinois Data Bank] Dataset successfully deposited')
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], bcc: 'databank@library.illinois.edu',  subject: '[Illinois Data Bank] Dataset successfully deposited')
     else
       Rails.logger.warn "Confirmation email not sent because dataset not found for key: #{dataset_key}."
     end
@@ -27,8 +27,30 @@ class DatabankMailer < ActionMailer::Base
     else
       mail(from: @params['help-email'] , to:['databank@library.illinois.edu'], subject:'[Illinois Data Bank] Help Request')
     end
+  end
 
+  def dataset_incomplete_1m(dataset_key)
+    if @dataset
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], bcc: 'databank@library.illinois.edu',  subject: '[Illinois Data Bank] Incomplete dataset deposit')
+    else
+      Rails.logger.warn "Dataset incomplete 1m email not sent because dataset not found for key: #{dataset_key}."
+    end
+  end
 
+  def embargo_approaching_1m(dataset_key)
+    if @dataset
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], bcc: 'databank@library.illinois.edu',  subject: '[Illinois Data Bank] Dataset embargo date approaching')
+    else
+      Rails.logger.warn "Embargo approaching 1m email not sent because dataset not found for key: #{dataset_key}."
+    end
+  end
+
+  def embargo_approaching_1w(dataset_key)
+    if @dataset
+      mail(to: [@dataset.depositor_email, @dataset.corresponding_creator_email], bcc: 'databank@library.illinois.edu',  subject: '[Illinois Data Bank] Dataset embargo date approaching')
+    else
+      Rails.logger.warn "Embargo approaching 1w email not sent because dataset not found for key: #{dataset_key}."
+    end
   end
 
 end
