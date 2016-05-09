edaba575c7af1ed144676375bb7d2efbe48aba1e
Colleen Fallaw <mfall3@illlinois.edu>
2016-05-09 10:28:54 -0500

    display version message when dataset has version relationship

--- app/models/related_material.rb
+++ app/models/related_material.rb
@@ -6,4 +6,14 @@ class RelatedMaterial < ActiveRecord::Base
   def as_json(options={})
     super(:only => [:material_type, :availability, :link, :uri, :uri_type, :citation, :dataset_id, :created_at, :updated_at])
   end
+
+  def relationship_arr
+    if self.datacite_list && self.datacite_list != ''
+      return self.datacite_list.split(',')
+    else
+      return Array.new
+    end
+
+  end
+
 end
