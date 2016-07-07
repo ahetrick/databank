1011e96af55f540999d8f73e2ac3cdff3f0cb4b4
Colleen Fallaw <mfall3@illlinois.edu>
2016-07-07 10:45:09 -0500

    fix suppress metadata embargoed dataset

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -436,7 +436,7 @@ class DatasetsController < ApplicationController
       respond_to do |format|
 
         if @dataset.save
-          if delete_datacite_id(@dataset, current_user)
+          if Dataset.delete_datacite_id(@dataset, current_user)
             @dataset.save
             format.html { redirect_to dataset_path(@dataset.key), notice: %Q[Reserved DOI has been deleted and all files have been permanently supressed.] }
             format.json { render :show, status: :ok, location: dataset_path(@dataset.key) }
