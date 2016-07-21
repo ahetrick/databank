92df0d5ff5780f68cf48b2464afab291555fa4c4
Colleen Fallaw <mfall3@illlinois.edu>
2016-07-21 15:14:57 -0500

    refine storage mount detection

--- config/initializers/file_mode.rb
+++ config/initializers/file_mode.rb
@@ -2,7 +2,7 @@ require 'pathname'
 
 Databank::Application.file_mode = Databank::FileMode::WRITE_READ
 
-mount_path = Pathname.new("IDB_CONFIG[:storage_mount]").realpath
+mount_path = Pathname.new(IDB_CONFIG[:storage_mount]).realpath
 
 if (mount_path.to_s.casecmp IDB_CONFIG[:read_write_realpath]) != 0
   Databank::Application.file_mode = Databank::FileMode::READ_ONLY
