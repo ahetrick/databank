3e77f588a2c391580257cda6581d4ee2ca9c63fa
Colleen Fallaw <mfall3@illlinois.edu>
2016-07-21 15:23:40 -0500

    refine storage mount detection

--- config/initializers/file_mode.rb
+++ config/initializers/file_mode.rb
@@ -4,7 +4,7 @@ Databank::Application.file_mode = Databank::FileMode::WRITE_READ
 
 mount_path = Pathname.new(IDB_CONFIG[:storage_mount]).realpath
 
-if (mount_path.to_s.casecmp IDB_CONFIG[:read_write_realpath]) != 0
+if (mount_path.to_s.casecmp IDB_CONFIG[:read_only_realpath]) == 0
   Databank::Application.file_mode = Databank::FileMode::READ_ONLY
   Databank::Application.alert_read_only_message = "Illinois Data Bank system is undergoing maintenance, and datasets cannot currently be added or edited."
 end
