2c7f84538fe6277385818b6b79a5aa755703d3c5
Colleen Fallaw <mfall3@illlinois.edu>
2016-07-21 15:11:18 -0500

    refine read-only mode detection

--- config/initializers/file_mode.rb
+++ config/initializers/file_mode.rb
@@ -2,13 +2,10 @@ require 'pathname'
 
 Databank::Application.file_mode = Databank::FileMode::WRITE_READ
 
-mount_path = Pathname.new(IDB_CONFIG[:staging_root]).realpath
-
-Rails.logger.warn "mount path: #{mount_path.to_s}"
-Rails.logger.warn IDB_CONFIG[:read_write_realpath]
+mount_path = Pathname.new("IDB_CONFIG[:storage_mount]").realpath
 
 if (mount_path.to_s.casecmp IDB_CONFIG[:read_write_realpath]) != 0
-  # Databank::Application.file_mode = Databank::FileMode::READ_ONLY
+  Databank::Application.file_mode = Databank::FileMode::READ_ONLY
   Databank::Application.alert_read_only_message = "Illinois Data Bank system is undergoing maintenance, and datasets cannot currently be added or edited."
 end
 
