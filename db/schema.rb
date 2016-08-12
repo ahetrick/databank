f16f4532f73a759de4f68fbcc34a4fe2d5e59d2a
Colleen Fallaw <mfall3@illlinois.edu>
2016-08-12 15:09:20 -0500

    increase limit on upload_file_size for datasets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812194655) do
+ActiveRecord::Schema.define(version: 20160812200636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 20160812194655) do
     t.string   "medusa_path"
     t.string   "binary_name"
     t.integer  "binary_size",          limit: 8
-    t.integer  "upload_file_size"
+    t.integer  "upload_file_size",     limit: 8
     t.string   "upload_status"
   end
 
