dd470df83562e481b2a75ebabc48f4050afdf36f
Colleen Fallaw <mfall3@illlinois.edu>
2016-10-13 13:14:41 -0500

    refine deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,8 +51,12 @@ set :assets_roles, [:web, :app]
 # set this to the number of versions to keep
 set :keep_assets, 2
 
+after 'deploy:publishing', 'deploy:restart'
+
 namespace :deploy do
-  task :restart, :roles => :web do
-    run "/home/databank/shared/scripts/full_restart.sh"
+  task :restart  do
+    on primary roles :web do
+      run "/home/databank/shared/scripts/full_restart.sh"
+    end
   end
 end
