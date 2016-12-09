11a52cc3f1b48cb5857891ab20a939bc0f7032ea
Colleen Fallaw <mfall3@illlinois.edu>
2016-12-09 15:38:49 -0600

    limit size for images

--- app/assets/javascripts/files.js
+++ app/assets/javascripts/files.js
@@ -302,7 +302,7 @@ function preview_image(web_id){
     
     $("#preview_" + web_id).show();
     if ($("#preview_" + web_id).is(':empty')){
-        $("#preview_" + web_id).html("<img src='/datafiles/" + web_id + "/display'>");
+        $("#preview_" + web_id).html("<img src='/datafiles/" + web_id + "/display' class='preview_body'>");
     }
     $("#preview_img_btn_" + web_id).html('<button type="button" class="btn btn-sm btn-success" onclick="hide_image_preview(&#39;' + web_id + '&#39;)"><span class="glyphicon glyphicon-eye-close"></span> Preview</button>');
 }
