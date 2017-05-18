5119164280628e7fb260c60a10057af1fb047afb
Colleen Fallaw <mfall3@illlinois.edu>
2017-05-18 12:27:06 -0500

    fetch iiif image in response to every view click

--- app/assets/javascripts/files.js
+++ app/assets/javascripts/files.js
@@ -301,12 +301,7 @@ function hide_preview(web_id){
 function preview_image(iiif_root, web_id){
     
     $("#preview_" + web_id).show();
-    if ($("#preview_" + web_id).is(':empty')){
-
-        //console.log(iiif_root + "/" + web_id + "/full/full/0/default.jpg");
-
-        $("#preview_" + web_id).html("<img src='" + iiif_root + "/" + web_id + "/full/full/0/default.jpg" + "' class='preview_body'>");
-    }
+    $("#preview_" + web_id).html("<img src='" + iiif_root + "/" + web_id + "/full/full/0/default.jpg" + "' class='preview_body'>");
     $("#preview_img_btn_" + web_id).html('<button type="button" class="btn btn-sm btn-success" onclick="hide_image_preview(&#39;' + web_id + '&#39;)"><span class="glyphicon glyphicon-eye-close"></span> View</button>');
 }
 
