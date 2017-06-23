31eeba8605827adbccf4beb44a5b6828e45f7b88
Colleen Fallaw <mfall3@illlinois.edu>
2017-06-23 13:20:22 -0500

    refine local development setup and documentation

--- idb_restart.sh
+++ idb_restart.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
-bundle exec passenger stop
-FILES=/tmp/RackMulti*
-for f in $FILES
-do
-  echo "Removing temporary file $f ..."
-  rm -f "$f"
-done
-
-bundle exec passenger start -d -e development -nginx-config-template nginx.conf.erb
+idb_stop.sh
+idb_start.sh
