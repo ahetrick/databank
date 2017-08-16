cc86a704eaf2a0defb0f87caef1337b34c42ec26
Colleen Fallaw <mfall3@illlinois.edu>
2017-08-16 14:54:36 -0500

    refine recordfile function

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -1235,7 +1235,7 @@ class DatasetsController < ApplicationController
 
       path_arr = Array.new
 
-      download_hash = DownloaderClient.datafiles_download_hash(web_ids, "DOI-#{@dataset.identifier}".parameterize)
+      download_hash = DownloaderClient.datafiles_download_hash(@dataset,web_ids, "DOI-#{@dataset.identifier}".parameterize)
       if download_hash
         if download_hash['status']== 'ok'
           web_ids.each do |web_id|
