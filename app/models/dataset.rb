c907a88635e6bae8faa9b6dac875d23d547fdc54
Colleen Fallaw <mfall3@illlinois.edu>
2017-09-07 13:18:18 -0500

    add debug code for failure to display a dataset

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -485,141 +485,179 @@ class Dataset < ActiveRecord::Base
 
   # making completion_check a class method with passed-in dataset, so it can be used by controller before save
   def self.completion_check(dataset, current_user)
-    response = 'ok'
-    validation_error_messages = Array.new
-    validation_error_message = ""
+    response = 'An unexpected exception was raised and logged during completion check.'
 
-    datafilesArr = Array.new
+    begin
+      validation_error_messages = Array.new
+      validation_error_message = ""
 
-    if !dataset.title || dataset.title.empty?
-      validation_error_messages << "title"
-    end
+      datafilesArr = Array.new
 
-    if dataset.creators.count < 1
-      validation_error_messages << "at least one creator"
-    end
-
-    if !dataset.license || dataset.license.empty?
-      validation_error_messages << "license"
-    end
-
-    contact = nil
-    dataset.creators.each do |creator|
-      if creator.is_contact?
-        contact = creator
+      if !dataset.title || dataset.title.empty?
+        validation_error_messages << "title"
       end
-    end
 
-    dataset.creators.each do |creator|
-      if !creator.email || creator.email == ''
-        validation_error_messages << "an email address for #{creator.given_name} #{creator.family_name}"
-      elsif creator.email.include?('@illinois.edu')
-        netid = creator.email.split('@').first
+      if dataset.creators.count < 1
+        validation_error_messages << "at least one creator"
+      end
 
-        creator_record = nil
+      if !dataset.license || dataset.license.empty?
+        validation_error_messages << "license"
+      end
 
-        #check to see if netid is found, to prevent email system errors
-        begin
-
-          creator_record = open("http://quest.grainger.uiuc.edu/directory/ed/person/#{netid}").read
-
-        rescue OpenURI::HTTPError => err
-          validation_error_messages << "a valid email address for #{creator.given_name} #{creator.family_name} (please check and correct the netid)"
+      contact = nil
+      dataset.creators.each do |creator|
+        if creator.is_contact?
+          contact = creator
         end
-
       end
-    end
 
-    dataset.creators.each do |creator|
-      if !creator.given_name || creator.given_name == ''
-        validation_error_messages << "at least one given name for author(s)"
-        break
+      dataset.creators.each do |creator|
+        if !creator.email || creator.email == ''
+          validation_error_messages << "an email address for #{creator.given_name} #{creator.family_name}"
+        elsif creator.email.include?('@illinois.edu')
+          netid = creator.email.split('@').first
+
+          creator_record = nil
+
+          #check to see if netid is found, to prevent email system errors
+          begin
+
+            creator_record = open("http://quest.grainger.uiuc.edu/directory/ed/person/#{netid}").read
+
+          rescue OpenURI::HTTPError => err
+            validation_error_messages << "a valid email address for #{creator.given_name} #{creator.family_name} (please check and correct the netid)"
+          end
+
+        end
       end
-    end
 
-    dataset.creators.each do |creator|
-      if !creator.given_name || creator.given_name == ''
-        validation_error_messages << "a family name for author(s)"
-        break
+      dataset.creators.each do |creator|
+        if !creator.given_name || creator.given_name == ''
+          validation_error_messages << "at least one given name for author(s)"
+          break
+        end
       end
-    end
 
-    unless contact
-      validation_error_messages << "select primary contact from author list"
-    end
-
-    if current_user
-      if ((current_user.role != 'admin') && (dataset.release_date && (dataset.release_date > (Date.current + 1.years))))
-        validation_error_messages << "a release date no more than one year in the future"
+      dataset.creators.each do |creator|
+        if !creator.given_name || creator.given_name == ''
+          validation_error_messages << "a family name for author(s)"
+          break
+        end
       end
-    end
 
-    if dataset.license && dataset.license == "deposit_agreement.txt"
-      has_file = false
-      if dataset.datafiles
-        dataset.datafiles.each do |datafile|
-          if datafile.bytestream_name && ((datafile.bytestream_name).downcase == "deposit_agreement.txt")
-            has_file = true
+      unless contact
+        validation_error_messages << "select primary contact from author list"
+      end
+
+      if current_user
+        if ((current_user.role != 'admin') && (dataset.release_date && (dataset.release_date > (Date.current + 1.years))))
+          validation_error_messages << "a release date no more than one year in the future"
+        end
+      end
+
+      if dataset.license && dataset.license == "deposit_agreement.txt"
+        has_file = false
+        if dataset.datafiles
+          dataset.datafiles.each do |datafile|
+            if datafile.bytestream_name && ((datafile.bytestream_name).downcase == "deposit_agreement.txt")
+              has_file = true
+            end
           end
         end
-      end
 
-      if !has_file
-        validation_error_messages << "a license file named deposit_agreement.txt or a different license selection"
-      end
-
-    end
-
-    if dataset.identifier && dataset.identifier != ''
-      dupcheck = Dataset.where(identifier: dataset.identifier)
-      if dupcheck.count > 1
-        validation_error_messages << "a unique DOI"
-      end
-    end
-
-    if dataset.datafiles.count < 1
-      validation_error_messages << "at least one file"
-    else
-      dataset.datafiles.each do |datafile|
-        datafilesArr << datafile.bytestream_name
-      end
-
-      firstDup = datafilesArr.detect { |e| datafilesArr.count(e) > 1 }
-
-      if firstDup
-        validation_error_messages << "no duplicate filenames (#{firstDup})"
-      end
-
-    end
-
-    if dataset.embargo && [Databank::PublicationState::Embargo::FILE, Databank::PublicationState::Embargo::METADATA].include?(dataset.embargo)
-      if !dataset.release_date || dataset.release_date <= Date.current
-        validation_error_messages << "a future release date for delayed publication (embargo) selection"
-      end
-
-    else
-      if dataset.release_date && dataset.release_date > Date.current
-        validation_error_messages << "a delayed publication (embargo) selection for a future release date"
-      end
-    end
-
-    if dataset.is_import? && !dataset.identifier
-      validation_error_messages << "identifier to import"
-    end
-
-    if validation_error_messages.length > 0
-      validation_error_message << "Required elements for a complete dataset missing: "
-      validation_error_messages.each_with_index do |m, i|
-        if i > 0
-          validation_error_message << ", "
+        if !has_file
+          validation_error_messages << "a license file named deposit_agreement.txt or a different license selection"
         end
-        validation_error_message << m
-      end
-      validation_error_message << "."
 
-      response = validation_error_message
+      end
+
+      if dataset.identifier && dataset.identifier != ''
+        dupcheck = Dataset.where(identifier: dataset.identifier)
+        if dupcheck.count > 1
+          validation_error_messages << "a unique DOI"
+        end
+      end
+
+      if dataset.datafiles.count < 1
+        validation_error_messages << "at least one file"
+      else
+        dataset.datafiles.each do |datafile|
+          datafilesArr << datafile.bytestream_name
+        end
+
+        firstDup = datafilesArr.detect { |e| datafilesArr.count(e) > 1 }
+
+        if firstDup
+          validation_error_messages << "no duplicate filenames (#{firstDup})"
+        end
+
+      end
+
+      if dataset.embargo && [Databank::PublicationState::Embargo::FILE, Databank::PublicationState::Embargo::METADATA].include?(dataset.embargo)
+        if !dataset.release_date || dataset.release_date <= Date.current
+          validation_error_messages << "a future release date for delayed publication (embargo) selection"
+        end
+
+      else
+        if dataset.release_date && dataset.release_date > Date.current
+          validation_error_messages << "a delayed publication (embargo) selection for a future release date"
+        end
+      end
+
+      if dataset.is_import? && !dataset.identifier
+        validation_error_messages << "identifier to import"
+      end
+
+
+    rescue Exception => exception
+
+      # temporary debugging strategy
+      # I expect something terrible is happening here at runtime,
+      # and my overall rescue of Standard Exception
+      # is not catching anything.
+      # This is totally the desparate measure it looks like.
+
+      Rails.logger.warn exception.to_yaml
+
+      exception_string = "*** Standard Error caught in application_controller.rb on #{IDB_CONFIG[:root_url_text]} ***\nclass: #{exception.class}\nmessage: #{exception.message}\n"
+      exception_string << Time.now.utc.iso8601
+
+      exception_string << "\nstack:\n"
+      exception.backtrace.each do |line|
+        exception_string << line
+        exception_string << "\n"
+      end
+
+      Rails.logger.warn(exception_string)
+
+      if current_user
+        exception_string << "\nCurrent User: #{current_user.name} | #{current_user.email}"
+      end
+
+      notification = DatabankMailer.error(exception_string)
+      notification.deliver_now
+
+      raise exception
+    else
+      if validation_error_messages.length > 0
+        validation_error_message << "Required elements for a complete dataset missing: "
+        validation_error_messages.each_with_index do |m, i|
+          if i > 0
+            validation_error_message << ", "
+          end
+          validation_error_message << m
+        end
+        validation_error_message << "."
+
+        response = validation_error_message
+      else
+        response = 'ok'
+      end
+    ensure
+      return response
     end
-    response
+    
   end
 
   def withdrawn_metadata
