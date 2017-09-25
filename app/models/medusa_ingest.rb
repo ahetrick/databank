4056739c537f1e03300f99572e6f2ce5c081f518
Colleen Fallaw <mfall3@illlinois.edu>
2017-09-25 10:58:44 -0500

    temporary comment out dataset_info file functionality

--- app/models/medusa_ingest.rb
+++ app/models/medusa_ingest.rb
@@ -133,37 +133,39 @@ class MedusaIngest < ActiveRecord::Base
     medusa_ingest.save
 
 
-    # remove old recordfile, if exists
+    # *** UNCOMMENT THIS TO ACTIVATE DATASET_INFO FILES
 
-    dataset.recordfile.delete if dataset.recordfile
-
-    # write recordfile
-
-    recordfilename = "dataset_info_#{file_time}.txt"
-    record_filepath = "#{staging_dir}/system/#{recordfilename}"
-
-    File.open(record_filepath, "w") do |recordfile|
-      recordfile.puts(dataset.recordtext)
-    end
-
-    recordfile = Recordfile.create(dataset_id: dataset.id)
-    recordfile.binary = Pathname.new(record_filepath).open
-    recordfile.binary_name = recordfile.binary.file.filename
-    recordfile.binary_size = recordfile.binary.size
-    recordfile.save
-
-    # make symlink, because setting as binary removes the file and puts it in uploads
-
-    FileUtils.ln(recordfile.bytestream_path, record_filepath)
-    FileUtils.chmod "u=wrx,go=rx", recordfile.bytestream_path
-
-    medusa_ingest = MedusaIngest.new
-    staging_path = "#{IDB_CONFIG[:dataset_staging]}/#{dataset_dirname}/system/#{recordfilename}"
-    medusa_ingest.staging_path = staging_path
-    medusa_ingest.idb_class = 'recordfile'
-    medusa_ingest.idb_identifier = recordfile.web_id
-    medusa_ingest.send_medusa_ingest_message(staging_path)
-    medusa_ingest.save
+    # # remove old recordfile, if exists
+    #
+    # dataset.recordfile.delete if dataset.recordfile
+    #
+    # # write recordfile
+    #
+    # recordfilename = "dataset_info_#{file_time}.txt"
+    # record_filepath = "#{staging_dir}/system/#{recordfilename}"
+    #
+    # File.open(record_filepath, "w") do |recordfile|
+    #   recordfile.puts(dataset.recordtext)
+    # end
+    #
+    # recordfile = Recordfile.create(dataset_id: dataset.id)
+    # recordfile.binary = Pathname.new(record_filepath).open
+    # recordfile.binary_name = recordfile.binary.file.filename
+    # recordfile.binary_size = recordfile.binary.size
+    # recordfile.save
+    #
+    # # make symlink, because setting as binary removes the file and puts it in uploads
+    #
+    # FileUtils.ln(recordfile.bytestream_path, record_filepath)
+    # FileUtils.chmod "u=wrx,go=rx", recordfile.bytestream_path
+    #
+    # medusa_ingest = MedusaIngest.new
+    # staging_path = "#{IDB_CONFIG[:dataset_staging]}/#{dataset_dirname}/system/#{recordfilename}"
+    # medusa_ingest.staging_path = staging_path
+    # medusa_ingest.idb_class = 'recordfile'
+    # medusa_ingest.idb_identifier = recordfile.web_id
+    # medusa_ingest.send_medusa_ingest_message(staging_path)
+    # medusa_ingest.save
 
   end
 
