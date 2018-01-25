b351c87ea14889188dfce43b14599b271faeff63
Colleen Fallaw <mfall3@illlinois.edu>
2018-01-25 14:47:03 -0600

    refine debug

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -596,9 +596,7 @@ class Dataset < ActiveRecord::Base
         has_file = false
         if dataset.datafiles
           dataset.datafiles.each do |datafile|
-            Rails.logger.warn("Filename: #{(datafile.bytestream_name).downcase}")
             if datafile.bytestream_name && ((datafile.bytestream_name).downcase == "license.txt")
-              Rails.logger.warn("DEBUG text compare are license.txt")
               has_file = true
             end
           end
@@ -606,6 +604,14 @@ class Dataset < ActiveRecord::Base
 
         if !has_file
           Rails.logger.warn("Inside does not have file: #{dataset.key}, #{dataset.license}")
+          dataset.datafiles.each do |datafile|
+            Rails.logger.warn("Filename: #{(datafile.bytestream_name).downcase}")
+            if datafile.bytestream_name && ((datafile.bytestream_name).downcase == "license.txt")
+              Rails.logger.warn("DEBUG text compare are license.txt")
+              has_file = true
+            end
+          end
+          raise("Gimme a stack trace")
           validation_error_messages << "a license file named license.txt or a different license selection"
         end
 
