b73fa41ce5e2b5fcefe6bcd6021648612d6762ef
Colleen Fallaw <mfall3@illlinois.edu>
2018-07-31 16:03:31 -0500

    refine initialization related to aws

--- config/initializers/databank.rb
+++ config/initializers/databank.rb
@@ -4,9 +4,12 @@ IDB_CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'databank.yml'))[Rai
 
 Application.storage_manager = StorageManager.new
 
-Aws.config.update({
-                      region: IDB_CONFIG[:aws][:region],
-                      credentials: Aws::Credentials.new(IDB_CONFIG[:aws][:access_key_id], IDB_CONFIG[:aws][:secret_access_key])
-                  })
+if IDB_CONFIG[:aws][:s3_mode] == 'true'
+  Aws.config.update({
+                        region: IDB_CONFIG[:aws][:region],
+                        credentials: Aws::Credentials.new(IDB_CONFIG[:aws][:access_key_id], IDB_CONFIG[:aws][:secret_access_key])
+                    })
 
-Application.aws_signer = Aws::S3::Presigner.new
+  Application.aws_signer = Aws::S3::Presigner.new
+end
+
