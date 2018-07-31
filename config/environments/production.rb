cdda302cfb55da5179fd94672da59bbb31c5fefc
Colleen Fallaw <mfall3@illlinois.edu>
2018-07-31 16:51:37 -0500

    refine mail config for pilot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
   # email stuff
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: 'outbound-relays.techservices.illinois.edu',
-      domain: 'outbound-relays.techservices.illinois.edu',
+      address: 'carbon.library.illinois.edu',
+      domain: 'carbon.library.illinois.edu',
       openssl_verify_mode: 'none'
   }
   config.action_mailer.perform_deliveries = true
