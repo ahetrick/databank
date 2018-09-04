2ee4bb1d9cc03d3d0a8f1b40048cf7e5fa54d410
Colleen Fallaw <mfall3@illlinois.edu>
2018-09-04 14:18:06 -0500

    refine updates to medusa ingest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904182242) do
+ActiveRecord::Schema.define(version: 20180904191649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -239,7 +239,7 @@ ActiveRecord::Schema.define(version: 20180904182242) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "medusa_dataset_dir"
-    t.string   "draft_key"
+    t.string   "staging_key"
     t.string   "medusa_key"
   end
 
