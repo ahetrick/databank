45536ec69cba1dace246a710e129f87f8db00423
Colleen Fallaw <mfall3@illlinois.edu>
2018-10-22 17:22:11 -0500

    refine debugging

--- app/controllers/datafiles_controller.rb
+++ app/controllers/datafiles_controller.rb
@@ -424,6 +424,9 @@ class DatafilesController < ApplicationController
 
   def set_dataset
 
+    Rails.logger.warn("inside set_dataset")
+    Rails.logger.warn(params)
+    
     @dataset = nil
 
     if !@datafile && params.has_key?(:id)
