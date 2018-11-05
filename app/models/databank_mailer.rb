85908847ae18959d5ebe2e19708a0f4a4488d7c0
Colleen Fallaw <mfall3@illlinois.edu>
2018-11-05 14:32:38 -0600

    add test to subject line for mail from aws-databank-prod

--- app/models/databank_mailer.rb
+++ app/models/databank_mailer.rb
@@ -208,6 +208,8 @@ class DatabankMailer < ActionMailer::Base
       subject.prepend("[LOCAL: ")
     elsif IDB_CONFIG[:root_url_text].include?("pilot")
       subject.prepend("[PILOT: ")
+    elsif IDB_CONFIG[:root_url_text].include?("aws")
+      subject.prepend("[TEST: ")
     else
       subject.prepend("[")
     end
