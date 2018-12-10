ef2d62d9cea00c5813a952e85c1cf762bab4b877
Colleen Fallaw <mfall3@illlinois.edu>
2018-12-10 14:37:05 -0600

    remove debug logging

--- app/models/datafile.rb
+++ app/models/datafile.rb
@@ -388,15 +388,7 @@ class Datafile < ActiveRecord::Base
       if IDB_CONFIG[:aws][:s3_mode]
         first_bytes = self.current_root.get_bytes(self.storage_key, 0, ALLOWED_DISPLAY_BYTES)
 
-        Rails.logger.warn "ALLOWED_DISPLAY_BYTES: #{ALLOWED_DISPLAY_BYTES}"
-        Rails.logger.warn "first_bytes.class: #{first_bytes.class}"
-        Rails.logger.warn "first_bytes.length: #{first_bytes.length}"
-
-        part_text_string = first_bytes.gets
-
-        Rails.logger.warn "part_text_string.class: #{part_text_string.class}"
-        Rails.logger.warn "part_text_string: #{part_text_string}"
-        Rails.logger.warn "part_text_string.length: #{part_text_string.length.to_s}"
+        part_text_string = first_bytes.string
 
       else
         File.open(self.filepath) do |file|
