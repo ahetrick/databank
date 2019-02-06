7f2f9280e6a9141aa802a696094d051d8e603bec
Colleen Fallaw <mfall3@illlinois.edu>
2019-02-06 11:24:16 -0600

    refine recaptcha handling

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -12,7 +12,7 @@ class HelpController < ApplicationController
   def help_mail
     if params.has_key?("nobots")
       # ignore the spam
-    elsif verify_recaptcha(message: 'MESSAGE NOT SENT: reCAPTCHA verification required')
+    elsif verify_recaptcha(message: 'MESSAGE NOT SENT: reCAPTCHA verification required', error_callback: fallback)
       help_request = DatabankMailer.contact_help(params)
       help_request.deliver_now
       redirect_to '/help', notice: "Your email has been sent to the Research Data Service Team. "
@@ -21,4 +21,12 @@ class HelpController < ApplicationController
     end
 
   end
+
+  def quarantine
+    # not quarantining yet - just passing through for now
+    help_request = DatabankMailer.contact_help(params)
+    help_request.deliver_now
+    redirect_to '/help', notice: "Your email has been sent to the Research Data Service Team. "
+  end
+
 end
