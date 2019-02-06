5c3ba4d064ff0e58eaed50386f46b4c5b4434ed0
Colleen Fallaw <mfall3@illlinois.edu>
2019-02-06 13:46:53 -0600

    refine recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,8 +2,4 @@
 Recaptcha.configure do |config|
   config.site_key  = IDB_CONFIG[:recaptcha][:site_key]
   config.secret_key = IDB_CONFIG[:recaptcha][:secret_key]
-  # Uncomment the following line if you are using a proxy server:
-  if IDB_CONFIG[:local_mode] != true
-    config.proxy = IDB_CONFIG[:root_url_text]
-  end
 end
