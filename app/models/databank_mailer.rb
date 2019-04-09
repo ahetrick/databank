1cb417ccfef1ce1025bd0f753ae7d4f54a859c72
Colleen Fallaw <mfall3@illinois.edu>
2019-04-09 15:29:11 -0500

    refine mailer class for clarity

--- app/models/databank_mailer.rb
+++ app/models/databank_mailer.rb
@@ -1,229 +1,158 @@
+# frozen_string_literal: true
+
 require 'open-uri'
 require 'open_uri_redirections'
 
+# defines and sends email
 class DatabankMailer < ActionMailer::Base
-  default from: "databank@library.illinois.edu"
+  default from: 'databank@library.illinois.edu'
 
   def confirm_deposit(dataset_key)
-
     @dataset = Dataset.find_by_key(dataset_key)
-
     if @dataset
-
-      subject = prepend_system_code("Illinois Data Bank] Dataset successfully deposited (#{@dataset.identifier})")
-      to_array = Array.new
+      subject_base = "Illinois Data Bank] Dataset deposited (#{@dataset.identifier})"
+      subject = prepend_system_code(subject_base)
+      to_array = []
       to_array << @dataset.depositor_email
-
       @dataset.creators.each do |creator|
         to_array << creator.email
       end
-
-      mail(to: to_array , cc: 'databank@library.illinois.edu', subject: subject)
+      mail(to: to_array, cc: 'databank@library.illinois.edu', subject: subject)
     else
-      Rails.logger.warn "Confirmation email not sent because dataset not found for key: #{dataset_key}."
+      Rails.logger.warn "Confirmation email not sent: #{dataset_key}."
     end
   end
 
   def confirm_deposit_update(dataset_key)
-
     @dataset = Dataset.find_by_key(dataset_key)
-
     if @dataset
-      subject = prepend_system_code("Illinois Data Bank] Dataset successfully updated (#{@dataset.identifier})")
+      subject_base = "Illinois Data Bank] Dataset updated (#{@dataset.identifier})"
+      subject = prepend_system_code(subject_base)
       mail(to: 'databank@library.illinois.edu', subject: subject)
     else
-      Rails.logger.warn "Update confirmation email not sent because dataset not found for key: #{dataset_key}."
+      Rails.logger.warn "Update confirmation email not sent: #{dataset_key}."
     end
   end
 
   def contact_help(params)
-
     subject = prepend_system_code('Illinois Data Bank] Help Request')
     @params = params
     if @params['help-topic'] == 'Dataset Consultation'
-      subject = prepend_system_code('Illinois Data Bank] Dataset Consultation Request')
+      subject_base = 'Illinois Data Bank] Dataset Consultation Request'
+      subject = prepend_system_code(subject_base)
     end
-    mail(from: @params['help-email'], to: ['databank@library.illinois.edu', @params['help-email']], subject: subject)
-
+    mail(from: @params['help-email'],
+         to: ['databank@library.illinois.edu',
+              @params['help-email']],
+         subject: subject)
   end
 
   def dataset_incomplete_1m(dataset_key)
-
-    subject = prepend_system_code('Illinois Data Bank] Incomplete dataset deposit')
-
+    subject_base = 'Illinois Data Bank] Incomplete dataset deposit'
+    subject = prepend_system_code(subject_base)
     @dataset = Dataset.find_by_key(dataset_key)
     if @dataset
-      mail(to: dataset.depositor_email, cc: 'databank@library.illinois.edu', subject: subject)
+      mail(to: dataset.depositor_email,
+           cc: 'databank@library.illinois.edu',
+           subject: subject)
     else
-      Rails.logger.warn "Dataset incomplete 1m email not sent because dataset not found for key: #{dataset_key}."
+      Rails.logger.warn "Dataset incomplete 1m email not sent: #{dataset_key}."
     end
   end
 
   def embargo_approaching_1m(dataset_key)
-
-    subject = prepend_system_code('Illinois Data Bank] Dataset release date approaching')
-
+    subject_base = 'Illinois Data Bank] Dataset release date approaching'
+    subject = prepend_system_code(subject_base)
     @dataset = Dataset.find_by_key(dataset_key)
     if @dataset
-      mail(to: @dataset.depositor_email, cc: 'databank@library.illinois.edu', subject: subject)
+      mail(to: @dataset.depositor_email,
+           cc: 'databank@library.illinois.edu',
+           subject: subject)
     else
-      Rails.logger.warn "Embargo approaching 1m email not sent because dataset not found for key: #{dataset_key}."
+      Rails.logger.warn "Embargo 1m email not sent: #{dataset_key}."
     end
   end
 
   def embargo_approaching_1w(dataset_key)
-
-    subject = prepend_system_code('Illinois Data Bank] Dataset release date approaching')
-
+    subject_base = 'Illinois Data Bank] Dataset release date approaching'
+    subject = prepend_system_code(subject_base)
     @dataset = Dataset.find_by_key(dataset_key)
     if @dataset
-      mail(to: @dataset.depositor_email, cc: 'databank@library.illinois.edu', subject: subject )
+      mail(to: @dataset.depositor_email,
+           cc: 'databank@library.illinois.edu',
+           subject: subject)
     else
-      Rails.logger.warn "Embargo approaching 1w email not sent because dataset not found for key: #{dataset_key}."
+      Rails.logger.warn "Embargo 1w email not sent: #{dataset_key}."
     end
   end
 
   def error(error_text)
     @error_text = error_text
-
     subject = prepend_system_code('Illinois Data Bank] System Error')
-
-    mail(to: "#{IDB_CONFIG[:tech_error_mail_list]}", subject: subject)
+    mail(to: IDB_CONFIG[:tech_error_mail_list].to_s, subject: subject)
   end
 
   def ezid_warnings(report)
     @report = report
-
     subject = prepend_system_code('Illinois Data Bank] EZID Differences Report')
-
-    mail(to: "#{IDB_CONFIG[:tech_error_mail_list]}", subject: subject)
+    mail(to: IDB_CONFIG[:tech_error_mail_list].to_s, subject: subject)
   end
 
-  def backup_report()
-
+  def backup_report
     @expected_path = "#{IDB_CONFIG[:databank_backup_root]}/#{Date.current}.sql"
 
-    @is_ok = File.exists?(@expected_path)
-
-    subject = nil
-
-    if @is_ok
-      subject = prepend_system_code("Illinois Data Bank] Backup exists for #{Date.current}")
+    if File.exist?(@expected_path)
+      subject_base = "Illinois Data Bank] Backup exists for #{Date.current}"
+      @is_ok == true
     else
-      subject = prepend_system_code("Illinois Data Bank] Backup NOT FOUND for #{Date.current}")
+      subject_base = "Illinois Data Bank] Backup NOT FOUND for #{Date.current}"
+      @is_ok == false
     end
-
-    mail(to: "#{IDB_CONFIG[:tech_error_mail_list]}", subject: subject)
-
+    subject = prepend_system_code(subject_base)
+    mail(to: IDB_CONFIG[:tech_error_mail_list].to_s, subject: subject)
   end
 
   def confirmation_not_sent(dataset_key, err)
-    subject = prepend_system_code('Illinois Data Bank] Dataset confirmation email not sent')
-
+    subject_base = 'Illinois Data Bank] Dataset confirmation email not sent'
+    subject = prepend_system_code(subject_base)
     @err = err
     @dataset = Dataset.find_by_key(dataset_key)
     if @dataset
-      mail(to: 'databank@library.illinois.edu', subject: subject )
+      mail(to: 'databank@library.illinois.edu', subject: subject)
     else
-      Rails.logger.warn "Confirmation email not sent email not sent because dataset not found for key: #{dataset_key}."
+      Rails.logger.warn "Confirmation email not sent email not sent \
+because dataset not found for key: #{dataset_key}."
     end
-
   end
 
   def account_activation(identity)
     @identity = identity
-    mail(to: @identity.email, subject: "Illinois Data Bank account activation")
+    mail(to: @identity.email, subject: 'Illinois Data Bank account activation')
   end
 
   def password_reset(identity)
     @identity = identity
-    mail(to: @identity.email, subject: "Illinois Data Bank password reset")
+    mail(to: @identity.email, subject: 'Illinois Data Bank password reset')
   end
 
-  def link_report()
-    subject = prepend_system_code('Illinois Data Bank] Related Materials Links Status Report')
-
-    datasets = Dataset.where(publication_state: [Databank::PublicationState::RELEASED, Databank::PublicationState::Embargo::FILE, Databank::PublicationState::TempSuppress::FILE, Databank::PublicationState::PermSuppress::FILE]).where(is_test: false)
-
-    @report = "<table border='1'><tr><th>DOI</th><th>Dataset_URL</th><th>Material_Type</th><th>Relationship</th><th>Material_URL</th><th>Status_Code</th></tr>"
-
-    datasets.each do |dataset|
-
-      dataset.related_materials.each do |material|
-
-        datacite_arr = Array.new
-
-        if material.datacite_list && material.datacite_list != ''
-          datacite_arr = material.datacite_list.split(',')
-        end
-
-        datacite_arr.each do |relationship|
-
-          if ['IsPreviousVersionOf','IsNewVersionOf'].exclude?(relationship)
-
-            if material.link && material.link != ""
-
-              the_status = "error"
-
-
-              begin
-                io_thing = open(material.link, allow_redirections: :all)
-
-                # The text of the status code is in [1]
-                the_status = io_thing.status[0]
-
-              rescue OpenURI::HTTPError => the_error
-
-                the_status = the_error.io.status[0] # => 3xx, 4xx, or 5xx
-
-              rescue Errno::ENOENT, URI::InvalidURIError  => err
-
-                the_status = "malformed url"
-
-              rescue Exception => run_err
-
-                puts run_err.message
-                puts run_err.class
-                the_status = "miscongured target"
-
-              end
-
-            end
-
-            @report = @report + "<tr><td>#{dataset.identifier}</td><td>#{IDB_CONFIG[:root_url_text]}/datasets/#{dataset.key}</td><td>#{material.selected_type}</td><td>#{relationship}</td><td>#{material.link}</td><td>#{the_status}</td></tr>"
-
-          end
-
-        end
-
-      end
-
-    end
-
-    @report = @report + "</table>"
-
-    mail(to: "#{IDB_CONFIG[:related_materials_report_list]}", subject: subject)
-
+  def link_report
+    subject_base = 'Illinois Data Bank] Related Materials Links Status Report'
+    subject = prepend_system_code(subject_base)
+    @report = RelatedMaterial.link_report
+    mail(to: IDB_CONFIG[:related_materials_report_list].to_s, subject: subject)
   end
-
 
   def prepend_system_code(subject)
     # Rails.logger.warn IDB_CONFIG[:root_url_text]
-    if IDB_CONFIG[:root_url_text].include?("dev")
-      subject.prepend("[DEV: ")
-    elsif IDB_CONFIG[:root_url_text].include?("localhost")
-      subject.prepend("[LOCAL: ")
-    elsif IDB_CONFIG[:root_url_text].include?("pilot")
-      subject.prepend("[PILOT: ")
-    elsif IDB_CONFIG[:root_url_text].include?("aws")
-      subject.prepend("[TEST: ")
+    if IDB_CONFIG[:root_url_text].include?('dev')
+      subject.prepend('[DEV: ')
+    elsif IDB_CONFIG[:root_url_text].include?('localhost')
+      subject.prepend('[LOCAL: ')
+    elsif IDB_CONFIG[:root_url_text].include?('aws-demo')
+      subject.prepend('[TEST: ')
     else
-      subject.prepend("[")
+      subject.prepend('[')
     end
-    return subject
+    subject
   end
-
-
-
 end
-
--- app/models/related_material.rb
+++ app/models/related_material.rb
@@ -1,19 +1,83 @@
+# frozen_string_literal: true
+
+require 'uri'
+
+# represents a related material as defined in DataCite metadata schema
 class RelatedMaterial < ActiveRecord::Base
   include ActiveModel::Serialization
   belongs_to :dataset
   audited associated_with: :dataset
 
-  def as_json(options={})
-    super(:only => [:material_type, :availability, :link, :uri, :uri_type, :citation, :dataset_id, :created_at, :updated_at])
+  def as_json(*)
+    super(only: %i[material_type
+                   availability
+                   link
+                   uri
+                   uri_type
+                   citation
+                   dataset_id
+                   created_at
+                   updated_at])
   end
 
   def relationship_arr
-    if self.datacite_list && self.datacite_list != ''
-      return self.datacite_list.split(',')
+    if datacite_list && datacite_list != ''
+      datacite_list.split(',')
     else
-      return Array.new
+      []
     end
-
   end
 
+  def nonversion_relationships
+    relationship_arr - %w[IsPreviousVersionOf IsNewVersionOf]
+  end
+
+  def link_status
+    unless nonversion_relationships.count.positive?
+      return 'no non-version related materials'
+    end
+
+    return 'no link' unless link
+
+    return 'invalid url' unless link =~ /\A#{URI.regexp(%w[http https])}\z/
+
+    link_attempt_status
+  end
+
+  def report_row
+    return '' unless nonversion_relationships.count.positive?
+
+    "<tr><td>#{dataset.identifier}</td>\
+<td>#{IDB_CONFIG[:root_url_text]}/datasets/#{dataset.key}</td>\
+<td>#{selected_type}</td><td>#{nonversion_relationships}</td>\
+<td>#{link}</td><td>#{link_status}</td></tr>"
+  end
+
+  # html string of link report table
+  def self.link_report
+    datasets = Dataset.where(is_test: false).select(&:metadata_public?)
+    report = "<table border='1'><tr>\
+<th>DOI</th><th>Dataset_URL</th><th>Material_Type</th><th>Relationship</th>\
+<th>Material_URL</th><th>Status_Code</th></tr>"
+    datasets.each do |dataset|
+      dataset.related_materials.each do |material|
+        report += material.report_row
+      end
+    end
+    report += '</table>'
+  end
+
+  private
+
+  # link status given a validly formatted link
+  def link_attempt_status
+    case Net::HTTP.get_response(URI.parse(link))
+    when Net::HTTPSuccess then
+      'ok'
+    else
+      'not responding'
+    end
+  rescue # Recover on DNS failures..
+    'invalid or not responding'
+  end
 end
