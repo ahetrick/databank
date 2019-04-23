2c901440fb5c01deeceb54a07f48541451e42240
Colleen Fallaw <mfall3@illinois.edu>
2019-04-23 14:37:36 -0500

    work-around dropped column for dev systems

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -1,6 +1,10 @@
 class Invitee < ActiveRecord::Base
   before_destroy :destroy_identity
 
+  def group
+    "reviewer"
+  end
+
   def destroy_identity
     identity = Identity.find_by_email(self.email)
     if identity
