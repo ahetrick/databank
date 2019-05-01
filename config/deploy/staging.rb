6b2a177a3bb3f394e34e98775974f965439ea6bd
Colleen Fallaw <mfall3@illinois.edu>
2019-05-01 13:48:02 -0500

    fix typo

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-cd# server-based syntax
+# server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
