5e175c6c41ee33db41e49e66e7d48820506906d0
Colleen Fallaw <mfall3@illinois.edu>
2019-05-01 11:32:50 -0500

    refine pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     active_featured_researchers = FeaturedResearcher.where(is_active: true)
     if active_featured_researchers.count > 0
-      @featured_researcher = active_featured_researchers.order("RANDOM()").first
+      @featured_researcher = active_featured_researchers.order(Arel.sql("RANDOM()")).first
     end
   end
 
