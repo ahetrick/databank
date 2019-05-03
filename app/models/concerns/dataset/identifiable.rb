31a6a3bf8b554a3befb6809fc9c15a9a63b03b93
Colleen Fallaw <mfall3@illinois.edu>
2019-05-03 14:01:05 -0500

    refine interaction with DataCite API

--- app/models/concerns/dataset/identifiable.rb
+++ app/models/concerns/dataset/identifiable.rb
@@ -105,7 +105,7 @@ module Identifiable
     return nil unless identifier_present?
 
     json_body = %Q({"data": {"id": "#{identifier}","type": "dois",)
-    json_body + %Q("attributes": {"event": "#{event}","doi": "#{identifier}","url": "#{databank_url}","xml": %Q('#{to_datacite_xml}')}})
+    json_body + %Q("attributes": {"event": "#{event}","doi": "#{identifier}","url": "#{databank_url}","xml": "#{to_datacite_xml.gsub("\"", "'")}"}})
   end
 
   def to_datacite_xml
