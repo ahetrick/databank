d92233b5f8e92e0f6b3073e9e7e226705a04dee6
Colleen Fallaw <mfall3@illinois.edu>
2019-05-03 12:24:44 -0500

    refine interaction with DataCite API

--- app/models/concerns/dataset/identifiable.rb
+++ app/models/concerns/dataset/identifiable.rb
@@ -483,7 +483,7 @@ module Identifiable
 
   class_methods do
     def post_to_datacite(identifier, json_body)
-      url = URI("#{URI_BASE}/#{identifier}")
+      url = URI("#{URI_BASE}/#{identifier.downcase}")
 
       puts "inside post to datacite"
       puts url
