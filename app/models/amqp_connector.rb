4d6008a05699b31940f2794701b96acad5cb5de9
Colleen Fallaw <mfall3@illinois.edu>
2019-07-11 13:07:57 -0500

    tweak interaction with rabbitmq

--- app/models/amqp_connector.rb
+++ app/models/amqp_connector.rb
@@ -14,6 +14,7 @@ class AmqpConnector < Object
 
   def reinitialize
     config = (AMQP_CONFIG || {}).symbolize_keys
+    Rails.logger.warn "amqp config:\n#{config}"
     config.merge!(recover_from_connection_close: true)
     self.known_queues = Set.new
     self.connection.close if self.connection
