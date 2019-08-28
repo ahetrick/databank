c89cd2885f95b62aa4cf5559824e33dd9a4ad086
Colleen Fallaw <mfall3@illinois.edu>
2019-08-28 13:46:49 -0500

    refine config for demo deploy

--- config/deploy/aws-demo.rb
+++ config/deploy/aws-demo.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'databank-demo.illinois.edu', user: 'databank', roles: %w{app db web}
+server 'databank-demo.library.illinois.edu', user: 'databank', roles: %w{app db web}
 
 set :rails_env, 'aws-demo'
 
